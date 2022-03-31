08f0ef4b7df6aed074e8a0b66c66e0237c88f0f5
caizhiguang <316763675@qq.com>
2022-03-31 15:29:04 +0800

    update sh file

--- start_yz_chain.sh
+++ start_yz_chain.sh
@@ -1,3 +1,3 @@
 docker run --name yz-chain-agent-node -d \
 -p 8888:8888 -p 9876:9876 \
-yz-chain-nft:v2.1.0 /bin/bash -c "nodeos --genesis-json  /yzchain/config/genesis.json --data-dir /yzchain/data/ --config-dir /yzchain/config/ --disable-replay-opts"
+yz-chain-nft:v2.1.0 /bin/bash -c "nodenft --genesis-json  /yzchain/config/genesis.json --data-dir /yzchain/data/ --config-dir /yzchain/config/ --disable-replay-opts"
